89f1c5e2a5dde72844c49af5e8bb9e4eab268fe5
Dr Dilek Celik <dilekcell40@gmail.com>
2025-06-28 18:26:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -123,6 +123,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 # ------ Explainer------
 st.subheader("⚙️ Explainer")
 import shap
+import matplotlib
 # Train your model (assuming xgb_model is already trained)
 explainer = shap.Explainer(model)
 # Get SHAP values
